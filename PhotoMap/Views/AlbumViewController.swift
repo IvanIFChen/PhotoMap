6d9e7ee1db4c6c3ba42de53e048d33dd0381bae1
Ivan Chen <ivan.if.chen@gmail.com>
2017-08-27 16:47:23 -0400

    setup dummy tab views; added app and tab icons

---
+++ PhotoMap/Views/AlbumViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewController.swift
+//  PhotoMap
+//
+//  Created by Ivan Chen on 8/24/17.
+//
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ PhotoMap/Views/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  PhotoMap
+//
+//  Created by Ivan Chen on 8/24/17.
+//
+//
+
+import UIKit
+
+class MapViewController: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
