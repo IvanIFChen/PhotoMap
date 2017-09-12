37b00226deb7852b1e0a3ecedde7083ef608346e
Ivan Chen <ivan.if.chen@gmail.com>
2017-09-12 02:43:23 -0400

    test cleanup and updated tab bar controller's test

--- PhotoMap/TabBarController.swift
+++ PhotoMap/TabBarController.swift
@@ -10,29 +10,9 @@ import UIKit
 
 class TabBarController: UITabBarController
 {
-    enum TabBarItemType: Int
-    {
-        case map
-        case camera
-        case album
-
-        func tabBarTitle() -> String
-        {
-            switch self
-            {
-                case .map:
-                    return "Map"
-                case .camera:
-                    return "Camera"
-                case .album:
-                    return "Album"
-            }
-        }
-    }
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        selectedIndex = TabBarItemType.camera.rawValue
+        selectedIndex = 0
     }
 }
--- PhotoMapTests/TabBarControllerTests.swift
+++ PhotoMapTests/TabBarControllerTests.swift
@@ -15,24 +15,13 @@ class TabBarControllerTests: XCTestCase
 
     override func setUp() {
         super.setUp()
-        sut = UITabBarController()
+        sut = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarControllerStoryBoardID") as? UITabBarController
         _ = sut.view
-//        sut.viewControllers?.forEach
-//        { (viewController) in
-//            _ = viewController.view
-//        }
     }
 
     override func tearDown() {
         sut = nil
         super.tearDown()
-    }
-
-    func testTabBarItemTypeTitle()
-    {
-        XCTAssertTrue(TabBarController.TabBarItemType.map.tabBarTitle() == "Map")
-        XCTAssertTrue(TabBarController.TabBarItemType.camera.tabBarTitle() == "Camera")
-        XCTAssertTrue(TabBarController.TabBarItemType.album.tabBarTitle() == "Album")
     }
 
     func testTabBarControllerTitles()
@@ -45,5 +34,12 @@ class TabBarControllerTests: XCTestCase
         XCTAssertTrue(tabs[0].title == "Map")
         XCTAssertTrue(tabs[1].title == "Camera")
         XCTAssertTrue(tabs[2].title == "Album")
+    }
+
+    func testCorrectChildViewControllers()
+    {
+        XCTAssertTrue(sut.childViewControllers[0] is MapViewController)
+        XCTAssertTrue(sut.childViewControllers[1] is CameraViewController)
+        XCTAssertTrue(sut.childViewControllers[2] is AlbumViewController)
     }
 }
