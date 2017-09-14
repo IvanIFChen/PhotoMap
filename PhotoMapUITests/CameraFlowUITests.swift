f52a765624266d102fa152aa8659f435e630b1d5
Ivan Chen <ivan.if.chen@gmail.com>
2017-09-14 19:17:19 -0400

    WIP camera flow ui test

--- PhotoMapUITests/CameraFlowUITests.swift
+++ PhotoMapUITests/CameraFlowUITests.swift
@@ -25,16 +25,18 @@ class CameraFlowUITests: XCTestCase
 
         app.cameraTab.tap()
 
-//        let app = XCUIApplication()
-//        let cameraButton = app.tabBars.buttons["Camera"]
-//        cameraButton.tap()
-//        
+        let app = XCUIApplication()
+        let cameraButton = app.tabBars.buttons["Camera"]
+        cameraButton.tap()
+//
         // swiftlint:disable line_length
-//        let element3 = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-//        let element = element3.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element
-//        element.tap()
+        let element3 = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        let element = element3.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element
+        element.tap()
         // swiftlint:enable line_length
         // question: how do I UI test this? Or even unit test this? (the image picker)
+
+
 
         // TODO check it's the camera view
     }
