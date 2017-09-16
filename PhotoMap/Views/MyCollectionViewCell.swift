5ad93f6f239008853bec182fe6249ccc02a13957
Ivan Chen <ivan.if.chen@gmail.com>
2017-09-16 19:08:03 -0400

    moved collection view cell

---
+++ PhotoMap/Views/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  PhotoMap
+//
+//  Created by Ivan Chen on 9/14/17.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var myLabel: UILabel!
+}
