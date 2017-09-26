6426644029146f52f057bac5a1c54f6f264c78b9
Ivan Chen <ivan.if.chen@gmail.com>
2017-09-26 02:42:03 -0400

    removed wip code in map view

--- PhotoMap/Views/MapViewController.swift
+++ PhotoMap/Views/MapViewController.swift
@@ -12,35 +12,12 @@ import CoreLocation
 
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
-    fileprivate let locationManager = CLLocationManager()
-    fileprivate let geocoder = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-    }
-}
-
-// MARK: - CLLocationManagerDelegate
-extension MapViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locations.forEach { location in
-//            print("##### This is the location: \(location.coordinate)")
-            geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, _) in
-                placemarks?.forEach { placemark in
-//                    print("@@@ This is the geocoded location: \(placemark)")
-                }
-            }
-            )
-        }
-        locationManager.stopUpdatingLocation()
     }
 }
