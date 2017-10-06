6d2473aeed3b644390ad5abe6cbebe6a0762109e
Ivan Chen <ivan.if.chen@gmail.com>
2017-10-06 00:11:42 -0400

    use core data to store snaps now

--- PhotoMap/Snap.swift
+++ PhotoMap/Snap.swift
@@ -9,74 +9,31 @@ import Foundation
 import UIKit
 import CoreLocation
 
-class Snap: NSObject, NSCoding
+class Snap
 {
-    var image: UIImage
-    var location: CLLocation
-    var placemark: CLPlacemark?
+    let image: UIImage
+    let latitude: Double
+    let longitude: Double
+    var address: String?
+//    var formattedAddressLines: [String]
 
-    static let documentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
-    static let archiveURL = documentsDirectory.appendingPathComponent("snaps")
-
-    override init()
-    {
-        self.image = UIImage()
-        self.location = CLLocation()
-    }
-
-    init(image: UIImage, location: CLLocation, placemark: CLPlacemark?)
+//    init(image: UIImage, latitude: Double, longitude: Double, formattedAddressLines: [String])
+    init(image: UIImage, latitude: Double, longitude: Double, address: String)
     {
         self.image = image
-        self.location = location
-        self.placemark = placemark
-
-        // NOTE: is it possible to reverse geocode in init? do it in another thread or something
-//        CLGeocoder().reverseGeocodeLocation(location, completionHandler:
-//        { (placemarks, _) in
-//            if let placemark = placemarks?.first
-//            {
-//               slef.placemark = placemark
-//            }
-//        })
+        self.latitude = latitude
+        self.longitude = longitude
+//        self.formattedAddressLines = formattedAddressLines
+        self.address = address
     }
 
     static func == (lhs: Snap, rhs: Snap) -> Bool
     {
         return UIImagePNGRepresentation(lhs.image) == UIImagePNGRepresentation(rhs.image) &&
-                lhs.location.coordinate.latitude == rhs.location.coordinate.latitude &&
-                lhs.location.coordinate.longitude == rhs.location.coordinate.longitude
-    }
-
-    // MARK: - NSCoding
-
-    struct PropertyKey
-    {
-        static let image = "image"
-        static let location = "location"
-        static let placemark = "placemark"
-    }
-
-    func encode(with aCoder: NSCoder)
-    {
-        aCoder.encode(image, forKey: PropertyKey.image)
-        aCoder.encode(location, forKey: PropertyKey.location)
-        aCoder.encode(placemark, forKey: PropertyKey.placemark)
-    }
-
-    required convenience init?(coder aDecoder: NSCoder)
-    {
-        guard let image = aDecoder.decodeObject(forKey: PropertyKey.image) as? UIImage else {
-            print("Unable to decode the image for a Snap object.")
-            return nil
-        }
-        guard let location = aDecoder.decodeObject(forKey: PropertyKey.location) as? CLLocation else {
-            print("Unable to decode the location for a Snap object.")
-            return nil
-        }
-
-        let placemark = aDecoder.decodeObject(forKey: PropertyKey.placemark) as? CLPlacemark
-
-        self.init(image: image, location: location, placemark: placemark)
+                lhs.latitude == rhs.latitude &&
+                lhs.longitude == rhs.longitude &&
+//                lhs.formattedAddressLines == rhs.formattedAddressLines
+                lhs.address == rhs.address
     }
 
 }
--- PhotoMap/Views/MapViewController.swift
+++ PhotoMap/Views/MapViewController.swift
@@ -19,5 +19,35 @@ class MapViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+
+        // CORE DATA TEST CDOE
+
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else
+//        {
+//            return
+//        }
+
+//        let context = appDelegate.persistentContainer.viewContext
+//
+//        let snap1 = SnapData(context: context)
+//        guard let imageData1 = UIImageJPEGRepresentation(UIImage(named: "meal1")!, 1.0) as NSData? else { return }
+//        snap1.image = imageData1
+//        snap1.latitude = 1.0
+//        snap1.longitude = 1.0
+//        snap1.address = "address 1"
+//
+//        let snap2 = SnapData(context: context)
+//        guard let imageData2 = UIImageJPEGRepresentation(UIImage(named: "meal2")!, 1.0) as NSData? else { return }
+//        snap2.image = imageData2
+//        snap2.latitude = 2.0
+//        snap2.longitude = 2.0
+//        snap2.address = "address 2"
+
+//        let task1 = Task(context: context)
+//        task1.name = "name1"
+//        let task2 = Task(context: context)
+//        task2.name = "name2"
+
+//        appDelegate.saveContext()
     }
 }
